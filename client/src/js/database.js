6723ebc22a57b3a4f8375bbf7955a0b746d0e279
SarahGormley <sarah.gormley39@gmail.com>
2022-08-08 20:13:34 +1000

    database.js added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,40 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log("Put to the database")
+
+  const connectDb = await openDB("jate", 1)
+
+  const trnsaction = connectDb.transaction("jate", "readwrite")
+
+  const store = trnsaction.objectStore("jate")
+
+  const req = store.put({ id: 1, value: content })
+
+  const res = await req
+  
+  console.log("Data saved to the database!", res)
+}
+
+export const getDb = async () => {
+  
+  console.log("Get from the database!")
+
+  const connectDb = await openDB("jate", 1)
+
+  const trnsaction = connectDb.transaction("jate", "readonly")
+
+  const store = trnsaction.objectStore("jate")
+
+  const req = store.getAll()
+
+  const res = await req
+
+  console.log("res.value", res)
+
+  return res?.value
+}
 
 initdb();
